791be410a8b556ea0509bb6a397479a80e600f0c
Guillermo Aguilar <gaguilar128@gmail.com>
2021-01-11 16:40:37 -0600

    Challenge 1

---
+++ Challenge 1/js/main.js
@@ -0,0 +1,25 @@
+// Please create a function that takes in two numbers. Print to the console the division of the these two numbers rounded down.
+//
+// I'll share how to submit this challenge tomorrow during class!
+
+
+// Example 1
+
+function challenge1(num1, num2){
+  let roundDown = num1 / num2
+  console.log(Math.floor(roundDown))
+}
+
+
+challenge1(100, 3)
+
+
+// Example 2
+//
+// function tryTwo(num1, num2){
+//   let roundDown = Math.floor(num1 / num2)
+//   console.log(roundDown)
+// }
+//
+//
+// tryTwo (5, 4)
