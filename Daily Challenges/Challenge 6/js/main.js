9c7397e39cb7460186be7a3dd538a606f619bb9d
Guillermo Aguilar <gaguilar128@gmail.com>
2021-01-16 00:00:51 -0600

    Daily Coding Challenge

---
+++ Daily Challenges/Challenge 6/js/main.js
@@ -0,0 +1,26 @@
+// // Please create a function that takes in a number. Print all the numbers from 1 to that number in the console, but skip any number that when multiplied by three is divisible by 2.
+
+
+
+function bigHead(num){
+  for(let i = 1; i <= num; i++){
+    if((i * 3) % 2 !== 0){
+    console.log(i)
+    }
+  }
+}
+
+bigHead(100)
+
+// or
+//
+//
+// function bigHead(num){
+//   for(let i = 1; i <= num; i++){
+//     if((i * 3) % 2 === 1){
+//     console.log(i)
+//     }
+//   }
+// }
+//
+// bigHead(100)
