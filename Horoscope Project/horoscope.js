72c50b0598c592c7ab72da28ef455dbf978a2f2c
Guillermo Aguilar <gaguilar128@gmail.com>
2021-01-18 11:41:03 -0600

    Horoscope Project

--- Horoscope Project/horoscope.js
+++ Horoscope Project/horoscope.js
@@ -2,8 +2,6 @@ document.querySelector('#button').addEventListener('click', horoscope)
 const selectMonth = document.querySelector('#Month')
 const selectDay = document.querySelector('#Day')
 
-// Incomplete, follow the same pattern for the rest of the dates
-
 function horoscope() {
   const month = selectMonth.value
   const day = Number(selectDay.value)
@@ -15,20 +13,20 @@ if(month === "December" && day >= 22 || month === "January" && day <= 19){
   return document.querySelector('.answer').innerText = 'Pisces'
 }else if(month === "March" && day >= 21 || month === "April" && day <= 19){
   return document.querySelector('.answer').innerText = 'Aries'
-}else if(month === "" && day >=  || month === "" && day <= ){
-  return document.querySelector('.answer').innerText = ''
-}else if(month === "" && day >=  || month === "" && day <= ){
-  return document.querySelector('.answer').innerText = ''
-}else if(month === "" && day >=  || month === "" && day <= ){
-  return document.querySelector('.answer').innerText = ''
-}else if(month === "" && day >=  || month === "" && day <= ){
-  return document.querySelector('.answer').innerText = ''
-}else if(month === "" && day >=  || month === "" && day <= ){
-  return document.querySelector('.answer').innerText = ''
-}else if(month === "" && day >=  || month === "" && day <= ){
-  return document.querySelector('.answer').innerText = ''
-}else if(month === "" && day >=  || month === "" && day <= ){
-  return document.querySelector('.answer').innerText = ''
+}else if(month === "April" && day >= 20 || month === "May" && day <= 20){
+  return document.querySelector('.answer').innerText = 'Taurus'
+}else if(month === "May" && day >= 21 || month === "June" && day <= 20){
+  return document.querySelector('.answer').innerText = 'Gemini'
+}else if(month === "June" && day >= 21 || month === "July" && day <= 22){
+  return document.querySelector('.answer').innerText = 'Cancer'
+}else if(month === "July" && day >= 23 || month === "August" && day <= 22){
+  return document.querySelector('.answer').innerText = 'Leo'
+}else if(month === "August" && day >= 23 || month === "September" && day <= 22){
+  return document.querySelector('.answer').innerText = 'Virgo'
+}else if(month === "September" && day >= 23 || month === "October" && day <= 22){
+  return document.querySelector('.answer').innerText = 'Libra'
+}else if(month === "October" && day >= 23 || month === "November" && day <= 21){
+  return document.querySelector('.answer').innerText = 'Scorpio'
 }else{
   return document.querySelector('.answer').innerText = 'Sagittarius'
 }
