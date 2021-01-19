2ae15b2df58fb04ea69eed9690203b377d90963b
Guillermo Aguilar <gaguilar128@gmail.com>
2021-01-19 15:18:06 -0600

    Task List Project

--- Task List Project/tasklist.js
+++ Task List Project/tasklist.js
@@ -5,11 +5,13 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-// Load all event listeners
 loadEventListeners();
 
 function loadEventListeners(){
-  form.addEventListener('submit', addTask);
+  form.addEventListener('submit', addTask)
+  taskList.addEventListener('click', removeTask)
+  clearBtn.addEventListener('click', clearTasks)
+  filter.addEventListener('keyup', filterTasks)
 }
 
 function addTask(e){
@@ -17,7 +19,7 @@ function addTask(e){
     alert('Add a task');
   }
   const li = document.createElement('li')
-  li.className = 'colletion-item'
+  li.className = 'collection-item'
   li.appendChild(document.createTextNode(taskInput.value))
   const link = document.createElement('a')
   link.className = 'delete-item secondary-content'
@@ -27,4 +29,32 @@ function addTask(e){
   taskInput.value = ''
 
   e.preventDefault();
+}
+
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove()
+    }
+  }
+}
+
+function clearTasks(){
+
+while(taskList.firstChild){
+  taskList.removeChild(taskList.firstChild)
+  }
+}
+
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block'
+    }else{
+      task.style.display = 'none'
+    }
+})
 }
