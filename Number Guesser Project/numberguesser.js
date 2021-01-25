a1ea5108217f023a80e8c3dd6da4ba03ec97ff73
Guillermo Aguilar <gaguilar128@gmail.com>
2021-01-25 12:48:58 -0600

    Number Guesser Project

--- Number Guesser Project/numberguesser.js
+++ Number Guesser Project/numberguesser.js
@@ -1,1 +1,21 @@
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
 
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+
+
+
+// document.querySelector('#guess-value').addEventListener('submit', checkNumber)
+//
+// function checkNumber(){
+//   let inputValue = document.querySelector('#guess-input').value
+//
+// }
