0b7636b5900806c4f4c750338ce4405e54635d71
Guillermo Aguilar <gaguilar128@gmail.com>
2021-02-01 16:30:48 -0600

    Number Guesser Project

--- Number Guesser Project/numberguesser.js
+++ Number Guesser Project/numberguesser.js
@@ -1,6 +1,6 @@
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 const game = document.querySelector('#game'),
@@ -12,6 +12,12 @@ const game = document.querySelector('#game'),
 
 minNum.textContent = min;
 maxNum.textContent = max;
+
+game.addEventListener('mousedown', function(e){
+  if(e.target.className === 'play-again'){
+    window.location.reload();
+  }
+})
 
 guessBtn.addEventListener('click',function(){
   let guess = parseInt(guessInput.value);
@@ -40,6 +46,12 @@ function gameOver(won, msg){
   guessInput.style.borderColor = color
   message.style.color = color
   setMessage(msg)
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again'
+}
+
+function getRandomNum(min, max){
+  return Math.floor(Math.random()* (max - min + 1) + min)
 }
 
  function setMessage(msg, color){
