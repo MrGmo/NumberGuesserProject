b503101307d63eed9b37637aefb248fc56229ec2
Guillermo Aguilar <gaguilar128@gmail.com>
2021-02-02 20:11:49 -0600

    Traversy Coding Challenges

--- Traversy JS Cardio Challenges/Session 1/main.js
+++ Traversy JS Cardio Challenges/Session 1/main.js
@@ -19,6 +19,27 @@
 //   return revString.reverse().join('')
 // }
 
+// Solution 3
+// function reverseString(str){
+//   let revString = ''
+//   for(let i = str.length-1; i >= 0; i--){
+//     revString = revString + str[i]
+//   }
+//   return revString
+// }
+
+// Solution 4
+
+// function reverseString(str){
+//   let revString = ''
+//   for(let i = 0; i < str.length; i++){
+//     revString = str[i] + revString
+//   }
+//   return revString
+// }
+
+// Solution 5
+
 
 
 console.log(reverseString('HELLO'))
@@ -27,40 +48,40 @@ console.log(reverseString('HELLO'))
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome ('hello') === 'false'
 
-function isPalindrome(){
-
-}
+// function isPalindrome(){
+//
+// }
 
 
 // CHALLENGE 3: REVERSE AN INTEGER
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int){
-
-}
+// function reverseInt(int){
+//
+// }
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str){
-
-}
+// function capitalizeLetters(str){
+//
+// }
 
 
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str){
-
-}
+// function maxCharacter(str){
+//
+// }
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-
-function fizzBuzz(){
-
-}
+//
+// function fizzBuzz(){
+//
+// }
