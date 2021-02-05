7fae84339d7eb7d4b83e3cb5fb05813a7c50e2c0
Guillermo Aguilar <gaguilar128@gmail.com>
2021-02-05 14:53:18 -0600

    API Homework

--- Object Practice/API Practice/NASA APOD API/main.js
+++ Object Practice/API Practice/NASA APOD API/main.js
@@ -10,9 +10,13 @@ function getPic(){
     .then(res => res.json())
     .then(data => {
       console.log(data)
-      document.querySelector('img').src = data.url
-      document.querySelector('h3').innerText = data.explanation
-      document.querySelector('h2').innerText = data.copyright
+      if(data.media_type === 'image'){
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
+        document.querySelector('h2').innerText = data.copyright
+      }else if(data.media_type === 'video'){
+        document.querySelector('iframe').src = data.url
+      }
     })
     .catch(err => {
       console.log(`error ${err}`)
