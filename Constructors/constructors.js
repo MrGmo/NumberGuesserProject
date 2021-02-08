8b32e5c48648918883904856606fdbd97a5c315e
Guillermo Aguilar <gaguilar128@gmail.com>
2021-02-08 00:47:38 -0600

    Constructors Practice

--- Constructors/constructors.js
+++ Constructors/constructors.js
@@ -1,5 +1,23 @@
 //Person Constructors
 
-function Person(){
-  this.name = 'Mike'
+function Person(name, dob){
+  this.name = name;
+  // this.age = 30;
+  this.birthday = new Date(dob)
+  this.calculateAge = function(){
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff)
+    return Math.abs(ageDate.getUTCFullYear() - 1970)
+  }
 }
+
+
+// const mike = new Person('Mike', 36)
+// const john = new Person('John', 30)
+//
+// console.log(john.age)
+
+const mike = new Person('Mike', '09/02/1988')
+
+
+console.log(mike.calculateAge())
