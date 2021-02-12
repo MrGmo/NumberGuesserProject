904891d15433db269cfc6523554e305ea2205180
Guillermo Aguilar <gaguilar128@gmail.com>
2021-02-12 15:48:07 -0600

    Coding Challenges

--- Traversy/ChallengesOne/challenge1.js
+++ Traversy/ChallengesOne/challenge1.js
@@ -3,6 +3,7 @@
 // Return a string in reverse
 // ex. reverseString('hello') === 'olleh'
 
+// Solution 1
 // function reverseString(str) {
 //   return str.split('').reverse().join('')
 // }
@@ -13,35 +14,44 @@
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str){
-  return str.split('').reverse().join('') === str
-}
-
-console.log(isPalindrome('racecar'))
+// Solution 1
+// function isPalindrome(str){
+//   return str.split('').reverse().join('') === str
+// }
+//
+// console.log(isPalindrome('racecar'))
 
 
 // CHALLENGE 3: REVERSE AN INTEGER
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int) {}
-
+// function reverseInt(int) {
+//   let string = int.toString().split('').reverse().join('')
+//   return parseInt(string)
+// }
+//
+// console.log(reverseInt(123))
 
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
 
-
+// Solution 1
+// function capitalizeLetters(str) {
+//   return str.split(' ').map(x => x[0].toUpperCase() + x.substring(1)).join(' ')
+// }
+//
+// console.log(capitalizeLetters('i love javascript'))
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+// function maxCharacter(str) {}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+// function fizzBuzz() {}
