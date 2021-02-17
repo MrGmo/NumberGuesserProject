857e3f747cf6d74bebf1433e6b6f7943f0271c17
Guillermo Aguilar <gaguilar128@gmail.com>
2021-02-17 14:20:19 -0600

    Coding Challenges

--- Traversy/ChallengesTwo/challenge2.js
+++ Traversy/ChallengesTwo/challenge2.js
@@ -1,20 +1,24 @@
 
 // CHALLENGE 1: LONGEST WORD
 // Return the longest word of a string
-// ex. longestWord('Hi there, my name is Brad') === 'there,'
+//If more than one longest word, put into an array
+// ex. longestWord('Hello, my name is Brad') === 'hello'
+//ex. longestWord('hello there, my name is Brad') === ['hello', 'there']
 
 // Solution 1
 // function longestWord(sen){
-//   return sen.split(' ').sort((a,b) => a.length - b.length).pop()
+//   const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g)
+//   const sortArr = wordArr.sort((a, b) => b.length - a.length)
+//   const longest = sortArr.filter(a => a.length === sortArr[0].length)
+//   if(longest.length === 1){
+//     return longest[0]
+//   }else{
+//     return longest
+//   }
 // }
-// console.log(longestWord('Guillermo your name is'))
+//
+// console.log(longestWord('Hello there, my name is Max'))
 
-// Solution 2
-
-
-// SOLUTION 1 - Return a single longest word
-// SOLUTION 2 - Return an array and include multiple words if they have the same length
-// SOLUTION 3 - Only return an array if multiple words, otherwise return a string
 
 
 // CHALLENGE 2: ARRAY CHUNKING
@@ -22,31 +26,70 @@
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-// function chunkArray(arr, len) {}
+// Solution 1
+// function chunkArray(arr, len){
+//   const chunkedArr = []
+//   let i = 0
+//
+//   while(i < arr.length){
+//     chunkedArr.push(arr.slice(i, i+len))
+//     i += len
+//   }
+//   return chunkedArr
+// }
+
+// Solution 2
+// function chunkArray(arr, len){
+//   const chunkedArr = []
+//
+//   arr.forEach(function(x){
+//     const last = chunkedArr[chunkedArr.length - 1]
+//     if(!last || last.length === len){
+//       chunkedArr.push([x])
+//     }else{
+//       last.push(x)
+//     }
+//   })
+//   return chunkedArr
+// }
+//
+// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2))
+
+
 
 // CHALLENGE 3: FLATTEN ARRAY
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
+// Solution 1
 // function flattenArray(arrays){
-//   let newArr = []
-//   for(let i = 0; i < arrays.length; i++){
-//     newArr.push(arrays[i])
-//   }
-//   return newArr
+//   return arrays.flat()
+// }
+
+// Solution 2
+// function flattenArray(arrays){
+//   return arrays.reduce((a, b) => a.concat(b))
+// }
+
+// Solution 3
+// function flattenArray(arrays){
+//   return [].concat.apply([], arrays)
 // }
 //
-//
-//
-//
-// console.log(flattenArray([1, 2], [3, 4], [5, 6], [7]))
+// console.log(flattenArray([[1, 2], [3, 4], [5, 6], [7]]))
 
 // CHALLENGE 4: ANAGRAM
 // Return true if anagram and false if not
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
+
+// function isAnagram(str1, str2){
 //
-// function isAnagram(str1, str2) {}
+// }
+//
+// console.log(isAnagram())
+
+
 
 // CHALLENGE 5: LETTER CHANGES
 // Change every letter of the string to the one that follows it and capitalize the vowels
